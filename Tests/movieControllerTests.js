b1d50577980361a43581232e803ff0d55ce4278d
Gideon Aduku <gideonaduku@gmail.com>
2017-04-27 15:28:26 +0000

    Update movieControllerTests.js

--- Tests/movieControllerTests.js
+++ Tests/movieControllerTests.js
@@ -8,7 +8,7 @@ describe('Movie Controller Tests:', ()=>{
 
             let req = {
                 body: {
-                    author: 'Jon'
+                    genre: 'Comedy'
                 }
             }
 
